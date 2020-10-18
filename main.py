09f9f554128a4a613a4b095d07f006d76920d6cd
hata_win <mh.mundus@gmail.com>
2020-10-18 22:45:50 +0900

    ready

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, redirect
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
